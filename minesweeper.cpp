6c17a7903b37ed022c60250645f17aa937f95812
Ondra Lupíšek <ondrej.lupisek@gmail.com>
2024-07-05 17:27:43 +0200

    added user input for size and mines

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,18 +1,24 @@
 #include <ncurses.h>
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
 //#define red COLOR_RED
 
 using namespace std;
 
 int main(int argc, char ** argv){
-    /*if(has_colors() == false){
-        endwin();
-        printf("no color terminal :(\n");
-        return 1;
-    }*/
+    using boost::lexical_cast;
+    using boost::bad_lexical_cast;
+
     int ch;
-    
+
+    int curx;
+    int cury;
+
+    char Swidth[10], Sheight[10], Smines[10];
+    int Iwidth, Iheight, Imines;
+
+    bool con;
     //screen init 
     initscr();
     raw();
@@ -23,37 +29,83 @@ int main(int argc, char ** argv){
     //init colors
     //inti_pair(1, COLOR_BLACK, COLOR_WIHTE)
 
-    printw("Hello World!\n");
-    //cout << "Hello World!\n";
+    printw("Hello, fellow minesweeper, I have a job for you!\n");
+    printw("So, what are you waiting for? Tell me what height, width and number of mines you want\n");
+    echo();
+
+    do{
+        try{
+            mvprintw(2, 0, "Height: ");
+            getstr(Sheight);
+            Iheight = boost::lexical_cast<int>(Sheight);
+            con = true;
+        }catch(bad_lexical_cast &){
+            con = false;
+            printw("nice try dishpit");
+        }
+    }while(!con);
+
+    do{
+        try{
+            mvprintw(3, 0, "Width: ");
+            getstr(Swidth);
+            Iwidth = boost::lexical_cast<int>(Swidth);
+            con = true;
+        }catch(bad_lexical_cast &){
+            con = false;
+            printw("nice try dishpit");
+        }
+    }while(!con);
+
+    do{
+        try{
+            mvprintw(4, 0, "Number of mines: ");
+            getstr(Smines);
+            Imines = boost::lexical_cast<int>(Smines);
+            con = true;
+        }catch(bad_lexical_cast &){
+            con = false;
+            printw("nice try dishpit");
+        }
+    }while(!con);
+
+    noecho();
+
+    clear();
+
+    printw("H:%d, W:%d, M:%d\n", Iheight, Iwidth, Imines);
+
+    curx = getcurx(stdscr);
+    cury = getcury(stdscr);
 
     refresh();
+
     do{
-    ch = getch();
-    printw("%c", ch);
-    printw("%2i", ch);
-    if(ch == 259){
-        printw("key up!");
-    }
-    printw("%2i", ch);
-    if(ch == 258){
-        printw("key down!");
-    }
+        printw("curx: %i, cury: %i", curx, cury);
+        ch = getch();
 
-   printw("%2i", ch);
-    if(ch == 260){
-        printw("key left!");
-    }
+        if(ch == 259){
+            printw("key up! ");
+            cury --;
+        } else if(ch == 258){
+            printw("key down! ");
+            cury ++;
+        } else if(ch == 260){
+            printw("key left! ");
+            curx --;
+        } else if(ch == 261){
+            printw("key right! ");
+            curx ++;
+        }                        
 
-   printw("%2i", ch);
-    if(ch == 261){
-        printw("key right!");
-    }
+        move(cury, curx);
+        refresh();
 
+    }while(ch != 27);
 
-    
-    }
-    while(ch != 65);
+    clear();
 
+    mvprintw(0, 0, "press any key to end program");
     getch();
 
     endwin();
